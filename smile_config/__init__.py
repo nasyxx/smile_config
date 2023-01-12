dcdb4b8038b4a142d3f37704c768bf2424a656e2
Nasy <nasyxx+git@gmail.com>
2023-01-12 17:49:47 -0600

    Explicitly export attribute

--- smile_config/__init__.py
+++ smile_config/__init__.py
@@ -9,4 +9,14 @@ from .api import (
     merge_dict,
 )
 
-__version__ = "0.9.13"
+__version__ = "0.9.14"
+
+__all__ = [
+    "Config",
+    "ConfigDict",
+    "Option",
+    "SConfig",
+    "from_dataclass",
+    "from_dict",
+    "merge_dict",
+]
